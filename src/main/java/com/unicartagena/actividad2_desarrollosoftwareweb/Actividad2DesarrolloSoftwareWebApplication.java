d490013d00b6226c9947b5ec0d13464324a7d1e3
Jonathan Tobon <jtobonc@unicartagena.edu.co>
2026-05-18 14:08:31 -0500

    Creacion del proyecto con Spring Boot

---
+++ src/main/java/com/unicartagena/actividad2_desarrollosoftwareweb/Actividad2DesarrolloSoftwareWebApplication.java
@@ -0,0 +1,13 @@
+package com.unicartagena.actividad2_desarrollosoftwareweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad2DesarrolloSoftwareWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Actividad2DesarrolloSoftwareWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unicartagena/actividad2_desarrollosoftwareweb/Actividad2DesarrolloSoftwareWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicartagena.actividad2_desarrollosoftwareweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad2DesarrolloSoftwareWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

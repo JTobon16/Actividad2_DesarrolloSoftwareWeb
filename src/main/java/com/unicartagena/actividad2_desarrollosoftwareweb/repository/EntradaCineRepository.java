5ef15edfad13849d17e4a454c117520be45b067c
Jonathan Tobon <jtobonc@unicartagena.edu.co>
2026-05-18 14:56:58 -0500

    feat: agregar repositorios User y EntradaCine

--- src/main/java/com/unicartagena/actividad2_desarrollosoftwareweb/repository/EntradaCineRepository.java
+++ src/main/java/com/unicartagena/actividad2_desarrollosoftwareweb/repository/EntradaCineRepository.java
@@ -1,4 +1,15 @@
 package com.unicartagena.actividad2_desarrollosoftwareweb.repository;
 
-public class EntradaCineRepository {
+import com.unicartagena.actividad2_desarrollosoftwareweb.model.EntradaCine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Le dice a Spring que esta interfaz es un repositorio
+@Repository
+public interface EntradaCineRepository extends JpaRepository<EntradaCine, String> {
+    // EntradaCine = modelo que maneja, String = tipo del ID
+
+    // Spring genera automaticamente el SQL:
+    // SELECT * FROM entrada_cine WHERE pelicula = ?
+    // (metodos personalizados se agregan aqui si se necesitan)
 }
--- src/main/java/com/unicartagena/actividad2_desarrollosoftwareweb/repository/UserRepository.java
+++ src/main/java/com/unicartagena/actividad2_desarrollosoftwareweb/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.unicartagena.actividad2_desarrollosoftwareweb.repository;
 
-public class UserRepository {
+import com.unicartagena.actividad2_desarrollosoftwareweb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+// Le dice a Spring que esta interfaz es un repositorio
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    // User = modelo que maneja, String = tipo del ID
+
+    // Spring genera automaticamente el SQL:
+    // SELECT * FROM users WHERE email = ?
+    Optional<User> findByEmail(String email);
 }
